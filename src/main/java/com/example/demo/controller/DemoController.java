4a1189e865a1b5f5075d58e6bffb052ccac332a2
cyb <280169928@qq.com>
2019-05-06 16:58:19 +0800

    init

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.annotation.RequiresGuest;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,10 +31,27 @@ public class DemoController {
     @ResponseBody
     @PostMapping("/login")
     public String postLogin(String username, String password) {
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        Subject currentUser = SecurityUtils.getSubject();
+        try {
+            currentUser.login(token);
+            currentUser.checkPermission("test");
+        } catch (UnknownAccountException uae) {
+            System.out.println("1");
+        } catch (IncorrectCredentialsException ice) {
+            System.out.println("2");
+        } catch (LockedAccountException lae) {
+            System.out.println("3");
+        } catch (ExcessiveAttemptsException eae) {
+            System.out.println("4");
+        } catch (AuthenticationException ae) {
+            System.out.println("5");
+        }
         return username + password;
     }
 
     @RequiresGuest
+    @ResponseBody
     @GetMapping("/hello1")
     public User demo1() {
         return userService.findUserByName("1111");
