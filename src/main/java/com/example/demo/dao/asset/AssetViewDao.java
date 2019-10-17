1d4e7efbfd2b6ef283d1e20a02882071ca4db61c
cyb <280169928@qq.com>
2019-10-17 16:58:26 +0800

    bug fix

--- src/main/java/com/example/demo/dao/asset/AssetViewDao.java
+++ src/main/java/com/example/demo/dao/asset/AssetViewDao.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AssetViewDao extends JpaRepository<AssetView, Long> {
-    List<AssetView> findByusernameOrOrderByCreateDate(String username);
+    List<AssetView> findByusernameOrderByCreateDate(String username);
 }
--- src/main/java/com/example/demo/service/asset/impl/AssetViewServiceImpl.java
+++ src/main/java/com/example/demo/service/asset/impl/AssetViewServiceImpl.java
@@ -17,7 +17,7 @@ public class AssetViewServiceImpl implements AssetViewService {
 
     @Override
     public List<AssetView> findViewDataByUserName(String userName) {
-        return accountingViewDao.findByusernameOrOrderByCreateDate(userName);
+        return accountingViewDao.findByusernameOrderByCreateDate(userName);
     }
 
 
